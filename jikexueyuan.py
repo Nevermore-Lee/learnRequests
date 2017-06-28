ee4626ac1222b42754323840d6a069a5b0c81c24
lihaosyd@gmail.com <lihaosyd@gmail.com>
2017-06-28 18:03:15 +0800

    work time commit three! 极客学院爬虫初试

---
+++ jikexueyuan.py
@@ -0,0 +1,54 @@
+import re
+import requests
+import sys
+
+class spider(object):
+    def __init__(self):
+        print('开始爬取内容...')
+
+    def getsource(self,url):
+        html = requests.get(url)
+        html.encoding='utf-8'
+        print(html.encoding)
+        return html.text
+
+    def changepage(self,url,total_page):
+        now_page = int(re.search('pageNum=(\d+)',url,re.S).group(1))
+        page_group = []
+        for i in range(now_page,total_page+1):
+            link = re.sub('pageNum=\d+','pageNum=%s'%i,url,re.S)
+            page_group.append(link)
+        return page_group
+
+    def geteveryclass(self,source):
+        everyclass = re.findall('(li id=".*?</li>)',source,re.S)
+        return everyclass
+
+    def getinfo(self,eachclass):
+        info={}
+        info['content'] = re.search('style="height: 0px; opacity: 0; display: none;">(.*?)</p>',eachclass,re.S).group(1)
+        info['title'] = re.search('class="lessonimg" title="(.*?)">', eachclass, re.S).group(1)
+        info['classtime'] = re.search('<em>(.*?)</em>', eachclass, re.S).group(1)
+        # info['title'] = re.search('title="(.*?)"alt=', eachclass, re.S).group(1)
+        return info
+    def saveinfo(self,classinfo):
+        f = open('info.txt','w',encoding='utf-8')
+        for each in classinfo:
+            f.writelines(('content:'+each['content']+'\n'))
+            f.writelines('title:'+each['title']+'\n')
+            f.writelines('classtime:'+each['classtime']+'\n')
+        f.close()
+if __name__=="__main__":
+    class_info = []
+    url = 'http://www.jikexueyuan.com/course/?pageNum=2'
+    jikespider = spider()
+    all_links = jikespider.changepage(url,3)
+    for link in all_links:
+        print("正在处理页面.."+link)
+        html = jikespider.getsource(link)
+        everyclass = jikespider.geteveryclass(html)
+        for each in everyclass:
+            info = jikespider.getinfo(each)
+            class_info.append(info)
+        jikespider.saveinfo(class_info)
+
--- learnRequests.py
+++ learnRequests.py
@@ -2,10 +2,17 @@ import requests
 import  re
 html = requests.get('http://www.toutiao.com/i6417563885221446146/')
 title = re.search('<title>(.*?)</title>',html.text,re.S).group(1)
+#爬起链接
 links = re.findall(' href="(.*?)"',html.text,re.S)
-print(html.text)
-print(html.encoding)
-print(title)
-for each in links:
+#爬去部分文字，先大后小
+text_field = re.findall('<div>(.*?)</div>',html.text,re.S)[0]
+print(text_field)
+the_text = re.findall('">(.*?)</a>',text_field,re.S)
+for each in the_text:
     print(each)
+# print(html.text)
+# print(html.encoding)
+# print(title)
+# for each in links:
+#     print(each)
 # print(links)
